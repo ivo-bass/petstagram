0afe1c66baee96f3ea736659ed97567d27136ba0
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-28 22:28:39 +0300

    added landing page

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from petstagram.common import views
 
+urlpatterns = [
+    path('', views.landing_page, name='landing')
 ]
